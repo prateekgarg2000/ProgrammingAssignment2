20fbca23357906e9931f4f38cd9df13c43c3af77
Prateek Garg <prateekgarg2208@gmail.com>
2020-05-25 18:58:41 +0530

    assignment 2 commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,26 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## this function returns the special matrix which basically is a list
+## this list contains the matrix 'x' and its inverse 'inverted_x'
+makeCacheMatrix <- function(x = matrix()){
+  list("x"=x,"inverted_x"=NULL)
 }
 
-
 ## Write a short comment describing this function
-
+## this function first checks if the inverse of x is initial stored or not
+## if not it calculates the inverse using solve
+## stores the inverse in the special matrix and returns it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## where x is a special matrix      
+  if(!is.null(x$inverted_x))
+  {
+    message("getting cached data")
+    return(x$inverted_x)
+  }
+  inverted_matrix<-solve(x$x,...)
+  x$inverted_x<<-inverted_matrix
+  inverted_matrix
+  
 }
